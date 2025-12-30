a3efab8cffcfb027b99c7f3c9ac427acaab2fc43
itsammar <hi@itsammar.com>
2025-12-30 18:49:09 -0500

    Landing Page: Made mini modifications

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,25 @@
 import Link from "next/link";
+import { Github, Linkedin, BookText, Menu } from "lucide-react";
 
 async function Navbar() {
   return (
     <div className="w-full py-6 px-4">
-      {/* Brand */}
-      <div className="navbar-left flex self-center items-center">
-        <h1 className="font-bold text-2xl mr-8">
+      {/* make mobile responsive */}
+      <div className="navbar-mobile flex justify-center self-center items-center md:hidden">
+        <div className="navbar-items flex gap-4 text-lg">
+          <div className="navbar-item cursor-pointer hover:text-teal-500 transition-colors duration-300">
+            <Link href="/" className="cursor-pointer">
+              <Menu /> <span className="sr-only">Menu</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* Desktop */}
+      <div className="navbar-left flex justify-between self-center items-center hidden md:flex">
+        <h1 className="font-bold text-3xl mr-8">
           it's <span className="text-teal-500">Ammar</span>
         </h1>
-        <div className="navbar-items flex gap-4">
+        <div className="navbar-items flex gap-4 text-lg">
           <div className="navbar-item">
             <Link href="/">Home</Link>
           </div>
@@ -25,6 +36,18 @@ async function Navbar() {
             <Link href="/">Projects</Link>
           </div>
         </div>
+        <div className="navbar-items flex gap-4 text-lg">
+          {/* Social Icons */}
+          <div className="navbar-item cursor-pointer">
+            <Link href="https://github.com/itsammar-dev"> <Github /> <span className="sr-only">GitHub</span> </Link>
+          </div>
+          <div className="navbar-item cursor-pointer">
+            <Link href="https://www.linkedin.com/in/itsammar-dev/"> <Linkedin /> <span className="sr-only">LinkedIn</span> </Link>
+          </div>
+          <div className="navbar-item cursor-pointer">
+            <Link href="https://medium.com/@ammarabdulrahman"> <BookText /> <span className="sr-only">Medium</span> </Link>
+          </div>
+        </div>  
       </div>
       <div className="navbar-right"></div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,12 @@ import Showcase from "./components/Showcase";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)] bg-gray-950 line-clamp-2">
+    <div className="font-[family-name:var(--font-geist-sans)] bg-gray-950 text-white min-h-screen">
       <main className="h-full">
         <div className="container mx-auto">
           <Navbar />
         </div>
         <Showcase />
-        {/* <h1>its Ammar. </h1> */}
       </main>
     </div>
   );
